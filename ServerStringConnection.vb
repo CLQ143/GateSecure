87ad4ca379ad17ccb3886d2945e2abff43621552
ravenscar06 <42130615+ravenscar06@users.noreply.github.com>
2018-12-20 20:15:23 +0800

    Add files via upload

---
+++ ServerStringConnection.vb
@@ -0,0 +1,16 @@
+﻿Public Class ServerStringConnection
+
+
+
+    'Dim s As String = "server= 192.168.254.102 ; user id= Exit ; password= '' ; database= vpapd"
+    'gamita ang password imong mysql na password instead na 1234 ------↓
+    Public s As String = "server= localhost; user id= root ; password='1234' ; database= vpapd"
+
+
+
+
+
+
+
+
+End Class
